50d3ed108cb99761ca49dc599b3c5e8cb81534cb
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-10-13 13:20:13 +0300

    update syntax

--- console.py
+++ console.py
@@ -188,7 +188,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
