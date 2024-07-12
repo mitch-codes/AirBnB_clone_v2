93dc29d144f04aee2ff3ff9eb8ebd4daaf2eeb14
mitch-codes <mitchkodes@gmail.com>
2024-07-12 07:30:20 +0000

    refactor all and delete function to act on individual objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        temp_obj_dict = {}
+        if cls == None:
+            return FileStorage.__objects
+            print(FileStorage.__objects)
+        else:
+            for j, k in FileStorage.__objects.items():
+                if type(k) == cls:
+                    temp_obj_dict[j] = k
+            return temp_obj_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +32,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+    def delete(self, obj=None):
+        """delete an object from dictionary __objects"""
+        if obj == None:
+            return
+        else:
+            temp_key = obj.__class__.__name__ + "." + obj.id
+            FileStorage.__objects.pop(temp_key)
 
     def reload(self):
         """Loads storage dictionary from file"""
