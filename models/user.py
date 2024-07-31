57ae2967470445c7e6a58875fb5f7671d48593fc
mitch-codes <mitchkodes@gmail.com>
2024-07-31 10:48:04 +0000

    make one to many relationship, users, places

--- models/user.py
+++ models/user.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False, primary_key=True)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship("Place", back_populates="users")
