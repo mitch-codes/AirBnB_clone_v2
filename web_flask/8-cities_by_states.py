08865a6cfb9cf34129a2ce5a842227f29c4d533b
mitch-codes <mitchkodes@gmail.com>
2024-09-03 07:06:06 +0000

    not tested yet

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,7 +4,9 @@ from flask inport Flask
 from flask import render_template
 from models import storage
 
-@app.route('/cities_by_states')
+app = Flask(__name__)
+
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     state_dict = {}
     all_states = stotage.all(State).values()
@@ -16,8 +18,11 @@ def cities_by_states():
                 tempcities.append(mycity)
         state_dict[mystate.id] = tempcities
 
-    return render_template('', states=all_states, state_dict=state_dict)
+    return render_template('templates/8-cities_by_states.html', states=all_states, state_dict=state_dict)
 
 @app.teardown_appcontext
 def db_close(exception):
     storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
